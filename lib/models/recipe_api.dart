8de6271f459b2aee5d82113700d22b9b15fd76f6
minhphan46 <21522345@gm.uit.edu.vn>
2022-12-23 23:25:35 +0700

    first

---
+++ lib/models/recipe_api.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:recipe_app/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+/* const unirest = require("unirest");
+
+const req = unirest("GET", "https://yummly2.p.rapidapi.com/feeds/list");
+
+req.query({
+	"limit": "24",
+	"start": "0"
+});
+
+req.headers({
+	"X-RapidAPI-Key": "1af9e36c00msh99f525155f26263p1c4d8bjsnaecf024205ff",
+	"X-RapidAPI-Host": "yummly2.p.rapidapi.com",
+	"useQueryString": true
+});
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+}); */
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https(
+        'yummly2.p.rapidapi.com', '/feeds/list', {"limit": "24", "start": "0"});
+    final response = await http.get(uri, headers: {
+      "X-RapidAPI-Key": "1af9e36c00msh99f525155f26263p1c4d8bjsnaecf024205ff",
+      "X-RapidAPI-Host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true",
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/recipe_api.dart';
+import '../models/recipe.dart';
+import '../widgets/recipe_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Recipe>? _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    getRecipes();
+    super.initState();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+    print(_recipes);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.restaurant_menu),
+              SizedBox(width: 10),
+              Text('Food Recipes'),
+            ],
+          ),
+        ),
+        body: RecipeCard(
+          title: 'My recipe',
+          rating: '4.9',
+          cookTime: '30 min',
+          thumbnailUrl:
+              'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+        ));
+  }
+}
